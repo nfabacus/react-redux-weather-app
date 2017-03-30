4e22354f441a8ea65cf25130a8b33d2213ab088b
Nobuyuki Fujioka <n.fujioka@abacusmaths.info>
2017-03-31 00:54:58 +0100

    WIP react-redux promise

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,8 +6,10 @@ const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${ API_K
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
 export function fetchWeather(city) {
-  const url = `${ROOT_URL}&q=${city},uk`;
+  const url = `${ROOT_URL}&q=${city},gb`;
   const request = axios.get(url);
+
+  console.log('Request:', request);
 
   return {
     type: FETCH_WEATHER,
