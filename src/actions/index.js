f78acc8a109a751d88afc7abc7647fa15db4d245
nobuyuki.fujioka <nobuyuki.fujioka@ecotricity.co.uk>
2017-03-30 14:00:23 +0100

    fixes api call

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const API_KEY = process.env.API_KEY;
-const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?appid=${ API_KEY }`;
+const API_KEY = "8e081a97ce3ce35b04ff7460e037fc27";
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${ API_KEY }`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
